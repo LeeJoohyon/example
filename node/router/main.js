bfc1ea14aef75883dd81a0b7290a13c7cc889b2d
Joo <jhe0956@gmail.com>
2019-06-19 01:03:16 +0900

    게시판 list page 작성 시작

--- node/router/main.js
+++ node/router/main.js
@@ -1,10 +1,38 @@
-module.exports = function(app)
-{
-    app.get('/',function(req,res){
-       res.render('index.html')
-    });
+var express = require("express");
+var router = express.Router(); // 
+var mysql = require("mysql");
+var fs = require("fs");
+var ejs = require("ejs");
+var bodyParser = require("body-parser");
 
-    app.get('/more',function(req,res){
-       res.render('about.html');
+router.use(bodyParser.urlencoded({ extended: false }));
+//게시판 페이징
+
+router.get("/list/:cur", function(req, res) {
+   var queryString = "select count(*) as cnt from post";
+   getConnection().query(queryString, function(err, data) {
+      if (err) {
+        console.log(err + "리스트 mysql 조회 실패");
+        return;
+      }else{
+         console.log(data);
+      }
    });
-}
+});
+
+
+var connection = mysql.createPool({
+   connectionLimit: 10,
+   host: 'testdb.chnpgohbb2gz.ap-northeast-2.rds.amazonaws.com',
+   user: 'root',
+   database: 'board',
+   password: '#bisu103702'
+ });
+ 
+ //디비 연결 함수
+ function getConnection() {
+   return connection;
+ }
+
+
+ module.exports = router;
--- node/server.js
+++ node/server.js
@@ -1,20 +1,18 @@
-var express = require('express');
+//모듈 임포트
+var express = require("express");
 var app = express();
-var router = require('./router/main')(app); 
-//라우터 모듈인 main.js 를 불러와서 app 에 전달해줍니다.
+var mysql = require("mysql"); //mysql 임포트
+var bodyParser = require("body-parser");
+//미들웨어 설정
+app.use(express.static("public")); //기본 파일 폴더 위치 설정
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('views', __dirname + '/views'); 
-//서버가 읽을 수 있도록 HTML 의 위치를 정의해줍니다.
-app.set('view engine', 'ejs'); 
-//서버가 HTML 렌더링을 할 때, EJS 엔진을 사용하도록 설정합니다.
-app.engine('html', require('ejs').renderFile); 
-//서버가 HTML 렌더링을 할 때, EJS 엔진을 사용하도록 설정합니다.
+//게시판
+var mainRouter = require("./router/main");
 
-// 만약 ejs 설치 에러가 났다면,
-// npm install ejs --save로 다시 ejs를 다시 설치하고 진행주시면 됩니다.
-
-app.use(express.static('public'));
-
-var server = app.listen(3000, function(){
-   console.log("Express server has started on port 3000")
+app.use(mainRouter);
+//var PORT = process.env.PORT || 3000
+//서버 가동
+app.listen(3000, function() {
+  console.log("서버가동");
 });
