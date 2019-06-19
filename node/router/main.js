deef05c89fa45644a92d098e1b995fb495937a77
james <jhe0956@gmail.com>
2019-06-19 12:29:27 +0900

    수요일 오늘 할분량부터

--- node/router/main.js
+++ node/router/main.js
@@ -9,29 +9,17 @@ router.use(bodyParser.urlencoded({ extended: false }));
 //게시판 페이징
 
 router.get("/list/:cur", function(req, res) {
-   // var queryString = "select count(*) as cnt from post";
 
-
-   // getConnection().query(queryString, function(err2, data) {
-   //    if (err2) {
-   //      console.log(err2 + "리스트 mysql 조회 실패");
-   //      return;
-   //    }
-   // });
-   var data = 1; 
    fs.readFile('list.html', 'utf-8', function (error, data) {
       res.send(ejs.render(data));
    });
       
-
-
-
-
-
-   //전체 게시물의 숫자
-   // totalPageCount = data[0].cnt
 });
 
+router.get("/", function(req,res){
+   console.log("메인화면");
+   res.redirect('/list/1');
+});
 
 // MYSQL 연결
 var connection = mysql.createPool({
